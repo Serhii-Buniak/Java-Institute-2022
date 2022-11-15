3d6f9413f8da1e4c700d298b7c70656eda76631e
Your Name <youremail@yourdomain.com>
2022-11-15 13:53:48 +0200

    space

--- lab1/src/Example.java
+++ lab1/src/Example.java
@@ -4,6 +4,7 @@ public class Example {
     public static void main(String[] args) {
         char inChar;
         System.out.println("Input symbol");
+        
         try {
             inChar = (char) System.in.read();
             System.out.println("Your symbol is " + inChar);
