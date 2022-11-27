036be1bb47ebdd48bf12fe840a078b5bffaf5434
Serhii Bunial hp-notebook <buniak.serhii@chnu.edu.ua>
2022-11-27 15:08:56 +0300

    lab17

---
+++ lab17/thymeleafApp/src/test/java/thymeleafApp/ThymeleafAppApplicationTests.java
@@ -0,0 +1,13 @@
+package thymeleafApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
