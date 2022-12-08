b9a7ded68cc1fea9264cc618a35c04beb1b5a104
Serhii Bunial hp-notebook <buniak.serhii@chnu.edu.ua>
2022-12-08 21:49:32 +0200

    lab19

---
+++ lab19/src/main/java/org/o7planning/sbgrovy/Lab19Application.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbgrovy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab19Application.class, args);
+	}
+
+}
---
+++ lab19/src/test/java/org/o7planning/sbgrovy/Lab19ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbgrovy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
